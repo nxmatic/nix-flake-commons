3b53b7505f964c7832c6728a8b71979257ebbba9
Stephane Lacoin (aka nxmatic) <stephane.lacoin@hyland.com>
2024-11-22 23:38:14 +0100

    shared common flake inputs

---
+++ flake.nix
@@ -0,0 +1,89 @@
+{
+  description = "nix system configurations";
+
+  nixConfig = {
+    substituters = [
+      "https://cache.nixos.org"
+      #      "https://kclejeune.cachix.org"
+      "https://cache.flox.dev"
+    ];
+
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      #      "kclejeuneachix.org-1:fOCrECygdFZKbMxHClhiTS6oowOkJ/I/dh9q9b1I4ko="
+      "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
+    ];
+  };
+
+  inputs = {
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    nix.url = "github:NixOS/nix/2.24.10";
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+    #   nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-24.05-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-24.11-darwin";
+    #   nixpkgs.url = "github:nxmatic/nixpkgs/develop";
+
+    cachix = {
+      url = "github:cachix/cachix";
+      inputs.flake-compat.follows = "flake-compat";
+    };
+
+    darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    devenv = {
+      url = "github:cachix/devenv/latest";
+      inputs.cachix.follows = "cachix";
+      inputs.flake-compat.follows = "flake-compat";
+      inputs.nix.follows = "nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flox = {
+      url = "github:flox/flox/v1.3.5";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+
+    # nxmatic 
+
+    bird = {
+      flake = true;
+      url = "github:nxmatic/bird?ref=hotfix/v2.15.1-nix-darwin";
+    };
+
+    maven-mvnd = {
+      flake = true;
+      url = "github:nxmatic/nix-maven-mvnd/develop";
+    };
+
+    nix-darwin-home = {
+      flake = true;
+      url = "github:nxmatic/nix-darwin-home/work";
+    };
+      
+    socket-vmnet = {
+      flake = true;
+      url = "github:nxmatic/nix-socket-vmnet/develop";
+    };
+
+    zen-browser = {
+      flake = true;
+      url = "github:nxmatic/nix-zen-browser/develop";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, home-manager, ... }:
+    {};
+
+}
